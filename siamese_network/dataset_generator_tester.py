0ac4aa241ba9329b1d9163d79f251c9486e3bd50
Nicolas <nicolas.six@pau.fr>
2018-06-24 14:33:41 -0400

    add a writer of generated pairs

---
+++ siamese_network/dataset_generator_tester.py
@@ -0,0 +1,43 @@
+import glob
+import os
+import cv2
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+
+from siamese_network.siamese import data_triple_generator_from_dir
+
+
+def main():
+    dataset_path = "data"
+    batch_size = 100
+    out_dir = "generated_pair_examples"
+    datagen = ImageDataGenerator(
+        featurewise_center=True,
+        featurewise_std_normalization=True,
+        rotation_range=5,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        zoom_range=[0.6, 1.1],
+        fill_mode='reflect',
+        horizontal_flip=True,
+        vertical_flip=False,
+        # validation_split=1.0 - train_ratio,
+        brightness_range=(0.7, 1.3)
+    )
+    train_images_paths = glob.glob(os.path.join("data", 'train/*/*/*'))
+    datagen.fit(np.array(list(map(cv2.imread, train_images_paths[:200]))))
+    # init triple generator (image 1, image 2, label)
+    triple_generator = data_triple_generator_from_dir(datagen,
+                                                      os.path.join(dataset_path, 'train'),
+                                                      batch_size,
+                                                      include_label=True)
+    os.makedirs(out_dir, exist_ok=True)
+    for i, (im1, im2, label) in enumerate(triple_generator):
+        if i > batch_size:
+            break
+        cv2.imwrite(os.path.join(out_dir, "{}_{:04d}_left.png".format(label, i)), im1)
+        cv2.imwrite(os.path.join(out_dir, "{}_{:04d}_right.png".format(label, i)), im2)
+
+
+if __name__ == '__main__':
+    main()
